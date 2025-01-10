41fc25a8a18091cb320f6f6ef986ef1516fdad0e
sirgicheha <samuelgicheha03@gmail.com>
2025-01-10 21:17:43 +0300

    Final Code Version

--- Tries_Dictionary.py
+++ Tries_Dictionary.py
@@ -2,12 +2,12 @@ import tkinter as tk
 from tkinter import Listbox, StringVar, messagebox
 
 class TrieNode:
-    def _init_(self):
+    def __init__(self):
         self.children = {}
         self.is_word = False
 
 class Trie:
-    def _init_(self):
+    def __init__(self):
         self.root = TrieNode()
 
     def insert(self, word):
@@ -51,8 +51,27 @@ class Trie:
         
         _remove(self.root, word, 0)
 
+    def suggest(self, prefix):
+        """Suggest all words in the Trie that start with the given prefix."""
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return []  # No suggestions if prefix not found
+            node = node.children[char]
+        
+        suggestions = []
+        self._collect_words(node, prefix, suggestions)
+        return suggestions
+
+    def _collect_words(self, node, prefix, suggestions):
+        """Helper function to collect words from the Trie."""
+        if node.is_word:
+            suggestions.append(prefix)
+        for char, child_node in node.children.items():
+            self._collect_words(child_node, prefix + char, suggestions)
+
 class DictionaryApp:
-    def _init_(self, root):
+    def __init__(self, root):
         self.root = root
         self.root.title("Dictionary with Autocomplete")
         
